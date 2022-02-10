d0e743c4ecca3b309193218a685c048a254ce4d6
hlh-restart <hlh@restart.be>
2022-02-10 16:17:19 +0100

    Some more static functions and replace each()

--- lib/adodb/adodb-error.inc.php
+++ lib/adodb/adodb-error.inc.php
@@ -103,7 +103,8 @@ function adodb_error_pg($errormsg)
 			 	 => DB_ERROR_ALREADY_EXISTS
         );
 	reset($error_regexps);
-    while (list($regexp,$code) = each($error_regexps)) {
+//  while (list($regexp,$code) = each($error_regexps)) {
+    foreach (error_regexps as $regexp => $code) {
         if (preg_match($regexp, $errormsg)) {
             return $code;
         }
